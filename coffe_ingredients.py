d52900997b770f6eccd3ce8b4b2f85ff5ade3443
automatik <michalslowikowski00@gmail.com>
2018-12-05 17:36:58 +0100

    coffee ingredients get set methods

--- coffe_ingredients.py
+++ coffe_ingredients.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 
 
 class CoffeeBeansTank:
@@ -10,12 +11,22 @@ class CoffeeBeansTank:
         if coffee >= 100:
             self.amount_of_coffee = coffee
         else:
-            print("No coffee, please fill the coffee beans")
+            pprint("No coffee, please fill the coffee beans")
             return False
 
 
 class WaterTank:
     amount_of_water: int = 1000
+
+    def getWater(self):
+        return self.amount_of_water
+
+    def setWater(self, water):
+        if water >= 250:
+            self.amount_of_water = water
+        else:
+            pprint("No water, please fill the water")
+            return False
 
 
 class Sugar:
--- coffee_maker.py
+++ coffee_maker.py
@@ -1,15 +1,11 @@
-# import coffee_beans_tank
-# import water_tank
-from logging import log, info, debug
 from pprint import pprint
-
 import coffe_ingredients
-# from msvcrt import getch
 
 
-class CoffeeMaker:  # TODO: dzidziczenie
-    water = coffe_ingredients.WaterTank()
+class CoffeeMaker:
     coffee = coffe_ingredients.CoffeeBeansTank()
+    water = coffe_ingredients.WaterTank()
+    get_coffee_ingredients = coffe_ingredients.CoffeeBeansTank()
 
     amount_of_water_for_espresso = 100
     amount_of_coffee_for_espresso = 15
@@ -23,30 +19,13 @@ class CoffeeMaker:  # TODO: dzidziczenie
         americano = "2. Americano"
         print(espresso + "\n" + americano)
 
-    # def press_button(self):
-    #
-    #     while True:
-    #         key = ord(getch())
-    #         if key == 27:  # ESC
-    #             break
-    #         elif key == 13:  # Enter
-    #             select()
-    #         elif key == 224:  # Special keys (arrows, f keys, ins, del, etc.)
-    #             key = ord(getch())
-    #             if key == 80:  # Down arrow
-    #                 moveDown()
-    #             elif key == 72:  # Up arrow
-    #                 moveUp()
-
     def make_coffee(self):
         """Make coffee based on customer order"""
 
-        get_coffee = coffe_ingredients.CoffeeBeansTank()
         self.display_coffee_menu()
         order = input("Choice > ").lower()
 
         if order == "espresso":
-            while order == "espresso":
             pprint("Checking coffee...")
             if self.water.amount_of_water < 100:
                 pprint("No water, Fill the watertank")
@@ -56,12 +35,15 @@ class CoffeeMaker:  # TODO: dzidziczenie
                 return False
             pprint("Preparing Espresso, please wait a moment")
             pprint("Coffee is ready, take your drink")
-            pprint(get_coffee.getCoffee())
+            pprint(self.get_coffee_ingredients.getCoffee())
 
-            self.new_amount = get_coffee.getCoffee()
-            self.new_amount = self.new_amount - self.amount_of_coffee_for_espresso
-            get_coffee.setCoffee(self.new_amount)
-            pprint(get_coffee.getCoffee())
+            #TODO: change var names
+            self.new_amount = self.get_coffee_ingredients.getCoffee()
+            new_amount = self.new_amount - self.amount_of_coffee_for_espresso
+
+            self.get_coffee_ingredients.setCoffee(new_amount)
+            pprint(self.get_coffee_ingredients.getCoffee())
+            #TODO: add new water amount var
             return True
 
         elif order == "americano":
