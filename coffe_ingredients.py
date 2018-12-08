60adfebcc834fb1eda2113c58b6a9893879d9197
automatik <michalslowikowski00@gmail.com>
2018-12-08 16:16:02 +0100

    arg for new amnt of coffee method, order changed to 1/2/3 choice, not string

--- coffe_ingredients.py
+++ coffe_ingredients.py
@@ -1,7 +1,13 @@
 
 
 class CoffeeBeansTank:
+    # TODO docstring
     amount_of_coffee: int = 1000
+    '''
+    r = dataBase
+    amount_of_coffee: int = f.read(coffeeBeans)
+    
+    '''
 
     def get_coffee(self):
         return self.amount_of_coffee
@@ -15,6 +21,8 @@ class CoffeeBeansTank:
 
 
 class WaterTank:
+    # TODO docstring
+    # Logic like in CoffeeBeansTank
     amount_of_water: int = 1000
 
 
@@ -24,3 +32,12 @@ class Sugar:
 
 class Milk:
     amount_of_milk = 1000
+
+
+def supply():
+    file = open("/Users/negativeone/Documents/programming/python/cwiczenia/coffee_maker/supply.txt")
+    print(file)
+
+
+if __name__ == "__main__":
+    supply()
--- coffee_maker.py
+++ coffee_maker.py
@@ -1,8 +1,8 @@
-from pprint import pprint
 import coffe_ingredients
 
 
 class CoffeeMaker:
+    # TODO docstring
     water = coffe_ingredients.WaterTank()
     coffee = coffe_ingredients.CoffeeBeansTank()
 
@@ -16,74 +16,75 @@ class CoffeeMaker:
     @staticmethod
     def display_coffee_menu():
         """Display available coffee for customer"""
-        try:
-            espresso = "1. Espresso"
-            americano = "2. Americano"
-            print(espresso + "\n" + americano)
-        except RuntimeError:
-            print("Menu was not displayed")
-
+        espresso = "1. Espresso"
+        americano = "2. Americano"
+        cancel = "3. Cancel"
+        print(espresso + "\n" + americano + "\n" + cancel)
 
     # TODO: add button for preparing coffee
+    # TODO: add data base for saving amount of water and coffee
 
     def espresso(self):
+        # TODO docstring
+
         if self.water.amount_of_water < self.water_for_espresso:
-            pprint("No water, Fill the watertank")
+            print("No water, Fill the water tank")
             return False
         elif self.coffee.amount_of_coffee < self.coffee_for_espresso:
-            pprint("No coffee beans, fill the coffeetank")
+            print("No coffee beans, fill the coffee tank")
             return False
         return True
 
     def americano(self):
+        # TODO docstring
         if self.water.amount_of_water < self.water_for_americano:
-            pprint("No water, Fill the watertank")
+            print("No water, Fill the water tank")
             return False
         elif self.coffee.amount_of_coffee < self.coffee_for_americano:
-            pprint("No coffee beans, fill the coffeetank")
+            print("No coffee beans, fill the coffee tank")
             return False
         return True
 
     def make_coffee(self):
         """Make coffee based on customer order"""
-        try:
-            self.display_coffee_menu()
-            order = input("Choice > ").lower()
-        except RuntimeError:
-            pprint("Something went wrong, please try again")
+        self.display_coffee_menu()
+        order = input("Choice > ").lower()
 
-        if order == "espresso":
-            pprint("Checking coffee...")
+        # print("Something went wrong, please try again")
+
+        if order == "1":
+            print("Checking coffee...")
             try:
                 self.espresso()
-            except RuntimeError:
-                pprint("No coffee selected")
-            pprint("Preparing Espresso, please wait a moment")
-            pprint("Coffee is ready, take your drink")
-            pprint(self.coffee.get_coffee())
-            self.set_new_values_for_coffee_ingredients()
+            except Exception as e:
+                print(e)
+            print("Preparing Espresso, please wait a moment")
+            print("Coffee is ready, take your drink")
+            print(self.coffee.get_coffee())
+            self.set_new_values_for_coffee_ingredients(self.coffee_for_espresso)
 
-        elif order == "americano":
-            pprint("Checking coffee...")
-            pprint("Coffee in tank: {0} grams".format(self.coffee.get_coffee()))
+        elif order == "2":
+            print("Checking coffee...")
+            print("Coffee in tank: {0} grams".format(self.coffee.get_coffee()))
             try:
                 self.americano()
             except RuntimeError:
-                pprint("No coffee selected")
-            pprint("Preparing Americano, please wait a moment")
-            pprint("Coffee is ready, take your drink")
-            self.set_new_values_for_coffee_ingredients()
-            pprint("Coffee left in tank after this coffee: {0} grams".format(self.coffee.get_coffee()))
+                print("No coffee selected")
+            print("Preparing Americano, please wait a moment")
+            print("Coffee is ready, take your drink")
+            self.set_new_values_for_coffee_ingredients(self.coffee_for_americano)
+            # print("Coffee left in tank after this coffee: {0} grams".format(self.coffee.get_coffee()))
+        elif order == "3":
+            print("Order was canceled")
         else:
-            pprint("No coffee selected. Please select coffee.")
-            print(" ")
-            self.make_coffee()
+            print("No coffee selected. Please select coffee.")
 
-    def set_new_values_for_coffee_ingredients(self):
+    def set_new_values_for_coffee_ingredients(self, amnt_of_coffee):
+        # TODO docstring
         new_amount_of_coffee = self.coffee.get_coffee()
-        new_amount_of_coffee = new_amount_of_coffee - self.coffee_for_espresso
+        new_amount_of_coffee -= amnt_of_coffee
         self.coffee.set_coffee(new_amount_of_coffee)
-        pprint(self.coffee.get_coffee())
+        print(self.coffee.get_coffee())
 
 
 if __name__ == "__main__":
