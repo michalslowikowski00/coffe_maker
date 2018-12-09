e0bf02ac73d039dce4245b8881d7c3329d96b503
automatik <michalslowikowski00@gmail.com>
2018-12-09 20:59:13 +0100

    extraction messeges to class

--- coffee_maker.py
+++ coffee_maker.py
@@ -26,9 +26,8 @@ class CoffeeMaker:
 
     def espresso(self):
         # TODO docstring
-
         if self.water.amount_of_water < self.water_for_espresso:
-            print("No water, Fill the water tank")
+            print("No water, fill the water tank")
             return False
         elif self.coffee.amount_of_coffee < self.coffee_for_espresso:
             print("No coffee beans, fill the coffee tank")
@@ -38,7 +37,7 @@ class CoffeeMaker:
     def americano(self):
         # TODO docstring
         if self.water.amount_of_water < self.water_for_americano:
-            print("No water, Fill the water tank")
+            print("No water, fill the water tank")
             return False
         elif self.coffee.amount_of_coffee < self.coffee_for_americano:
             print("No coffee beans, fill the coffee tank")
@@ -50,14 +49,9 @@ class CoffeeMaker:
         self.display_coffee_menu()
         order = input("Choice > ").lower()
 
-        # print("Something went wrong, please try again")
-
         if order == "1":
             print("Checking coffee...")
-            try:
-                self.espresso()
-            except Exception as e:
-                print(e)
+            self.espresso()
             print("Preparing Espresso, please wait a moment")
             print("Coffee is ready, take your drink")
             print(self.coffee.get_coffee())
@@ -66,10 +60,7 @@ class CoffeeMaker:
         elif order == "2":
             print("Checking coffee...")
             print("Coffee in tank: {0} grams".format(self.coffee.get_coffee()))
-            try:
-                self.americano()
-            except RuntimeError:
-                print("No coffee selected")
+            self.americano()
             print("Preparing Americano, please wait a moment")
             print("Coffee is ready, take your drink")
             self.set_new_values_for_coffee_ingredients(self.coffee_for_americano)
@@ -84,7 +75,7 @@ class CoffeeMaker:
         new_amount_of_coffee = self.coffee.get_coffee()
         new_amount_of_coffee -= amnt_of_coffee
         self.coffee.set_coffee(new_amount_of_coffee)
-        print(self.coffee.get_coffee())
+        # print(self.coffee.get_coffee())
 
 
 if __name__ == "__main__":
