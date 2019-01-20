f71324ee182f8223291a34c0b9e5eb91774d219d
automatik <michalslowikowski00@gmail.com>
2019-01-20 17:19:01 +0100

    one method for make_coffee

--- Coffee_Maker/coffee_maker.py
+++ Coffee_Maker/coffee_maker.py
@@ -15,21 +15,16 @@ class CoffeeMaker(Menu, Ingredient.NewIngredients, Ingredient.Coffee, Ingredient
         if order == "1":
             espresso = Espresso()
             espresso.make_coffee(espresso.bean, espresso.water)
-            print(Messages.checking_coffee)
-            print(Messages.preparing_espresso)
-            print(Messages.coffee_is_ready)
             self.set_new_values_for_coffee_ingredients(Ingredient.Coffee.coffee_for_espresso)
             self.set_new_values_for_water_ingredients(Ingredient.Coffee.water_for_espresso)
             print(Ingredient.Beans.get_coffee(self))
             print(Ingredient.Water.get_water(self))
+            print(espresso)
             return espresso
             # TODO set new value to data base
 
         elif order == "2":
             americano = Americano()
-            print(Messages.checking_coffee)
-            print(Messages.preparing_americano)
-            print(Messages.coffee_is_ready)
             self.set_new_values_for_coffee_ingredients(Ingredient.Coffee.coffee_for_americano)
             self.set_new_values_for_water_ingredients(Ingredient.Coffee.water_for_americano)
             return americano
