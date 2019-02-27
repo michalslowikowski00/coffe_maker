e1f3fce702f5127badcf3b2e2dfa3eabf4e970f2
automatik <michalslowikowski00@gmail.com>
2019-02-27 21:50:20 +0100

    fighting with enum in menu

--- app/coffee_maker.py
+++ app/coffee_maker.py
@@ -5,26 +5,32 @@ from exception.custom_exception import Message
 
 
 class CoffeeMaker(Menu, Ingredient):
+
     def make_coffee(self):
         """Make coffee based on customer order"""
-        self.display_coffee_menu()
-        order = input("Order: ")
+        menu = Menu()
         espresso = Espresso()
         americano = Americano()
         bean_container = BeanContainer()
         water_container = WaterContainer()
 
+        menu.display_coffee_menu()
+        order = input("Order: ")
+
+        # if order == menu.menu[1]:
         if order == "1":
             espresso.make_coffee(espresso.bean, espresso.water)
             bean_container.set_new_values_for_coffee_ingredients(espresso.bean)
             water_container.set_new_values_for_water_ingredients(espresso.water)
             return espresso
+        # elif order == menu.menu[2]:
         elif order == "2":
             print(water_container.water)
             americano.make_coffee(americano.bean, americano.water)
             bean_container.set_new_values_for_coffee_ingredients(americano.bean)
             water_container.set_new_values_for_water_ingredients(americano.water)
             return americano
+        # elif order == menu.menu[3]:
         elif order == "3":
             print(Message.canceled_order_message)
         else:
