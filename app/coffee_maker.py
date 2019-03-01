611fb78ff39c6273a01b3ceae9f948803b3761e3
michał słowikowski <michal.slowikowski@polidea.com>
2019-03-01 15:30:20 +0100

    rename coffee_maker to brew_coffee, espresso & americano objects added to if blocks, new menu object

--- app/coffee_maker.py
+++ app/coffee_maker.py
@@ -1,25 +1,26 @@
+from Exception.custom_exception import Message
+from Menu.menu import Menu
 from coffee.coffee import Espresso, Americano
-from ingredient.ingredient import Ingredient, BeanContainer, WaterContainer
-from menu.menu import Menu
-from exception.custom_exception import Message
+from ingredient.ingredient import BeanContainer, WaterContainer
 
 
-class CoffeeMaker(Menu, Ingredient):
-    def make_coffee(self):
+class CoffeeMaker():
+    def brew_coffee(self):
         """Make coffee based on customer order"""
-        self.display_coffee_menu()
+        Menu.display_coffee_menu()
         order = input("Order: ")
-        espresso = Espresso()
-        americano = Americano()
+
         bean_container = BeanContainer()
         water_container = WaterContainer()
 
         if order == "1":
+            espresso = Espresso()
             espresso.make_coffee(espresso.bean, espresso.water)
             bean_container.set_new_values_for_coffee_ingredients(espresso.bean)
             water_container.set_new_values_for_water_ingredients(espresso.water)
             return espresso
         elif order == "2":
+            americano = Americano()
             print(water_container.water)
             americano.make_coffee(americano.bean, americano.water)
             bean_container.set_new_values_for_coffee_ingredients(americano.bean)
@@ -34,4 +35,4 @@ class CoffeeMaker(Menu, Ingredient):
 if __name__ == "__main__":
     app = CoffeeMaker()
     while True:
-        app.make_coffee()
+        app.brew_coffee()
