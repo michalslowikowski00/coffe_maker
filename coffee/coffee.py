0ea05002ea29e2d9db6c8d8dc773d9095c121d28
michał słowikowski <michal.slowikowski@polidea.com>
2019-03-01 21:10:20 +0100

    fixed import

--- coffee/coffee.py
+++ coffee/coffee.py
@@ -1,6 +1,7 @@
 """ Module with two types of coffee -- espresso and americano."""
-from Exception.custom_exception import NoBeanError, NoWaterError, Message
+from exception.custom_exception import NoBeanError, NoWaterError, Message
 from ingredient.ingredient import Ingredient
+
 
 class Coffee:
     """Abstract class with one method for checking if
