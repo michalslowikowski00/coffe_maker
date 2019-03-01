3ce22d1686b4c44a86956eaef80c868d5b23ab10
michał słowikowski <michal.slowikowski@polidea.com>
2019-03-01 21:12:09 +0100

    added Order class with enum, Menu store display_menu only

--- menu/menu.py
+++ menu/menu.py
@@ -1,15 +1,19 @@
 from enum import Enum
 
 
-class Menu(Enum):
+class Order(Enum):
 
     ESPRESSO = 1
     AMERICANO = 2
     CANCEL = 3
 
-    def display_coffee_menu(self):
+
+class Menu:
+
+    espresso = "1. Espresso"
+    americano = "2. Americano"
+    cancel = "3. Cancel"
+
+    def display_menu(self):
         """Display available menu for customer"""
-        espresso = "1. Espresso"
-        americano = "2. Americano"
-        cancel = "3. Cancel"
-        print(espresso + "\n" + americano + "\n" + cancel)
+        print(self.espresso + "\n" + self.americano + "\n" + self.cancel)
