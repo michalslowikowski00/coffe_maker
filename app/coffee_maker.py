2993f3dcb704d01650345dc8f9b7741826718c3f
michał słowikowski <michal.slowikowski@polidea.com>
2019-03-01 21:09:36 +0100

    menu instance, brew coffee if stat. changed

--- app/coffee_maker.py
+++ app/coffee_maker.py
@@ -1,33 +1,33 @@
-from Exception.custom_exception import Message
-from Menu.menu import Menu
+from exception.custom_exception import Message
+from menu.menu import Menu, Order
 from coffee.coffee import Espresso, Americano
 from ingredient.ingredient import BeanContainer, WaterContainer
 
 
 class CoffeeMaker:
+
     def brew_coffee(self):
         """Make coffee based on customer order"""
         menu = Menu()
-        menu.display_coffee_menu()
-        order = input("Order: ")
+        menu.display_menu()
+        coffee = int(input("Select coffee: "))
 
         bean_container = BeanContainer()
         water_container = WaterContainer()
 
-        if order == "1":
+        if coffee == Order.ESPRESSO.value:
             espresso = Espresso()
             espresso.make_coffee(espresso.bean, espresso.water)
-            bean_container.set_new_values_for_coffee_ingredients(espresso.bean)
-            water_container.set_new_values_for_water_ingredients(espresso.water)
+            # bean_container.set_new_values_for_coffee_ingredients(espresso.bean)
+            # water_container.set_new_values_for_water_ingredients(espresso.water)
             return espresso
-        elif order == "2":
+        elif coffee == Order.AMERICANO.value:
             americano = Americano()
-            print(water_container.water)
             americano.make_coffee(americano.bean, americano.water)
-            bean_container.set_new_values_for_coffee_ingredients(americano.bean)
-            water_container.set_new_values_for_water_ingredients(americano.water)
+            # bean_container.set_new_values_for_coffee_ingredients(americano.bean)
+            # water_container.set_new_values_for_water_ingredients(americano.water)
             return americano
-        elif order == "3":
+        elif coffee == Order.CANCEL.value:
             print(Message.canceled_order_message)
         else:
             print(Message.no_coffee_selected_message)
